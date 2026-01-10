137288b28961c98276bc5dbd0785d5a3b579efa8
deepakchauhan33 <deep651965@gmail.com>
2026-01-10 23:09:49 +0530

    Write Js function that add itemss in the product section

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const Product = document.querySelectorAll('.product');
+const ProductCant = document.querySelector('.productCant');
+const loadBtn = document.querySelector('.loadBtn');
 
+loadBtn.addEventListener('click', getProducts);
 
-function card(data){
+function card(data) {
 
-    const box  = document.createElement('div');
-    const image  = document.createElement('img');
+    const box = document.createElement('div');
+    const image = document.createElement('img');
     const heading = document.createElement('h3');
     const price = document.createElement('span');
     const rating = document.createElement('p');
 
+    image.src = data.thumbnail;
+    image.alt = data.title;
+
     heading.innerText = data.title;
+    price.innerText = data.price;
+    rating.innerText = data.rating;
+
+    box.appendChild(image);
+    box.appendChild(heading);
+    box.appendChild(price)
+    box.appendChild(rating)
 
 
+    return box;
 }
 
 
-async function getData(){
-    
-    try{
-        const res = await axios.get("https://dummyjson.com/products");
-        const productList = res.data.products;
-        
-        productList.forEach(product => {
-           body.appendChild(createCard(product));
-        });
-    }catch(err){
+async function getProducts() {
+    try {
+
+        const item = await axios.get("https://dummyjson.com/products");
+        const products = item.data.products;
+
+        console.log(products);
+
+
+        products.slice(0, 10).forEach(product => {
+          ProductCant.appendChild(card(product))
+        })
+    } catch (err) {
         console.log(err);
     }
 }
-
-getData();
